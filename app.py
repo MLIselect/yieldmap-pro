9bc68bdabec35b79f0b4e5ba43533839f68a7b29
MLIselect <daniel.lambrou.sales@gmail.com>
2026-01-01 19:08:42 +0000

    update 3

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ hide_st_style = """
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             header {visibility: hidden;}
+            .stAppDeployButton {display:none;}
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
